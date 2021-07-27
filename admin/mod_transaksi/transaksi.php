afafda30e35fa7008d7247bd2fbffe4176abbdf6
ianaf1 <79218845+ianaf1@users.noreply.github.com>
2021-07-27 13:13:18 +0700

    Update transaksi.php

--- admin/mod_transaksi/transaksi.php
+++ admin/mod_transaksi/transaksi.php
@@ -163,7 +163,7 @@ $saldoawal = $saldolama;
                     $query = mysqli_query($koneksi, "select * from bulan where '$id_bulan[id_bulan]'");
                     $bulan = mysqli_fetch_array($query);
                     $namabulan = $bulan['nama_bulan'];
-                    echo "<h5 class='m-0 font-weight-bold text-primary'>Data Transaksi</h5>";
+                    echo "<h5 class='m-0 font-weight-bold text-primary'>Data Transaksi $namabulan</h5>";
                 }
                 ?>
                 <div class="card-header-action">
