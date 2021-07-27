6aa0ff118e9a1b0ac3203c47742484a63ce4001c
ianaf1 <79218845+ianaf1@users.noreply.github.com>
2021-07-27 17:46:17 +0700

    Update bayar.php

--- user/mod_bayar/bayar.php
+++ user/mod_bayar/bayar.php
@@ -5,7 +5,7 @@
             <div class="card-header">
                 <h4>Data biaya</h4>
                 <div class="card-header-action">
-                    <?php $query = mysqli_query($koneksi, "select sum(jumlah) as total from biaya a join daftar b ON a.id_kelas=b.kelas where a.id_kelas='$siswa[kelas]'");
+                    <?php $query = mysqli_query($koneksi, "select sum(jumlah) as total from biaya a join daftar b ON a.id_kelas=b.kelas where '$siswa[kelas]'=a.id_kelas");
                     $total = mysqli_fetch_array($query);
                     ?>
                     <b>Total Biaya Rp. <?= $total['total'] ?></b>
@@ -26,7 +26,7 @@
                         </thead>
                         <tbody>
                             <?php
-                            $query = mysqli_query($koneksi, "select * from biaya a join daftar b ON a.id_kelas=b.kelas where a.id_kelas='$siswa[kelas]'");
+                            $query = mysqli_query($koneksi, "select * from biaya a join daftar b ON a.id_kelas=b.kelas where '$siswa[kelas]'=a.id_kelas");
                             $no = 0;
                             while ($biaya = mysqli_fetch_array($query)) {
                                 $no++;
