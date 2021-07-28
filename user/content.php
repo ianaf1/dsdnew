771e19aa5f49f86120d41f32e71694c8f70ecff8
ianaf1 <79218845+ianaf1@users.noreply.github.com>
2021-07-28 21:20:11 +0700

    Update content.php

--- user/content.php
+++ user/content.php
@@ -1,7 +1,7 @@
 <?php defined('BASEPATH') or die("ip anda sudah tercatat oleh sistem kami") ?>
 <?php
 if ($pg == '') {
-    include "pagecontent.php";
+    include "v_pagecontent.php";
 } elseif ($pg == 'formulir') {
     include "mod_formulir/formulir.php";
 } elseif ($pg == 'detail') {
