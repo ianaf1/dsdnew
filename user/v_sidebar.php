6aaa8715c4f6c19d1d706330d24220edacd928ae
ianaf1 <79218845+ianaf1@users.noreply.github.com>
2021-07-28 21:12:11 +0700

    update

--- user/v_sidebar.php
+++ user/v_sidebar.php
@@ -28,16 +28,29 @@
 
     <!-- Nav Item - Pages Master Menu -->
     <div>
-        <li class="dropdown ">
-            <a href="#" class="nav-link has-dropdown"><i class="fas fa-fire fa-fw"></i> <span>Profil</span></a>
-            <ul class="dropdown-menu">
-                <li><a class="nav-link" href="?pg=detail">Lihat Profil</a></li>
-                <li><a class="nav-link" href="?pg=formulir">Edit Profil</a></li>
-            </ul>
+        <li class="nav-item">
+            <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseProfil" aria-expanded="true" aria-controls="collapseTwo">
+                <i class="fas fa-money-bill"></i>
+                <span>Profil</span>
+            </a>
+            <div id="collapseProfil" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
+                <div class="bg-white py-2 collapse-inner rounded">
+                    <h6 class="collapse-header">Data Keuangan</h6>
+                    <a class="collapse-item" href="?pg=detail">Lihat Profil</a>
+                    <a class="collapse-item" href="?pg=formulir">Edit Profil</a>
+                </div>
+            </div>
         </li>
-
-        <li><a class="nav-link" href="?pg=bayar"><i class="fas fa-money-bill    "></i> <span>Pembayaran</span></a></li>
-        <li><a class="nav-link" href="?pg=pengumuman"><i class="fas fa-bullhorn fa-fw"></i> <span>Pengumuman</span></a></li>
+        <li class="nav-item">
+            <a class="nav-link" href="?pg=bayar"">
+                <i class=" fas fa-user-friends"></i>
+                <span>Pembayaran</span></a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="?pg=pengumuman">
+                <i class="fas fa-user-friends"></i>
+                <span>Pengumuman</span></a>
+        </li>
     </div>
 
 
--- user/v_topbar.php
+++ user/v_topbar.php
@@ -35,7 +35,7 @@
         <!-- Nav Item - User Information -->
         <li class="nav-item dropdown no-arrow">
             <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                <span class="mr-2 d-none d-lg-inline text-gray-600 small"><b><?= $user['nama_user'] ?></b></span>
+                <span class="mr-2 d-none d-lg-inline text-gray-600 small"><b><?= $siswa['nama'] ?></b></span>
                 <img class="img-profile rounded-circle" src="../assets/img/avatar/avatar1.png">
             </a>
             <!-- Dropdown - User Information -->
