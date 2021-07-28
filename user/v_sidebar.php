cb2da9e67db0a01177b94c98118563ad40abed19
ianaf1 <79218845+ianaf1@users.noreply.github.com>
2021-07-28 21:30:08 +0700

    Update v_sidebar.php

--- user/v_sidebar.php
+++ user/v_sidebar.php
@@ -30,12 +30,12 @@
     <div>
         <li class="nav-item">
             <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseProfil" aria-expanded="true" aria-controls="collapseTwo">
-                <i class="fas fa-money-bill"></i>
+                <i class="fas fa-user-friends"></i>
                 <span>Profil</span>
             </a>
             <div id="collapseProfil" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
                 <div class="bg-white py-2 collapse-inner rounded">
-                    <h6 class="collapse-header">Data Keuangan</h6>
+                    <h6 class="collapse-header">Data Profil</h6>
                     <a class="collapse-item" href="?pg=detail">Lihat Profil</a>
                     <a class="collapse-item" href="?pg=formulir">Edit Profil</a>
                 </div>
@@ -43,7 +43,7 @@
         </li>
         <li class="nav-item">
             <a class="nav-link" href="?pg=bayar"">
-                <i class=" fas fa-user-friends"></i>
+                <i class=" fas fa-money-bill"></i>
                 <span>Pembayaran</span></a>
         </li>
         <li class="nav-item">
