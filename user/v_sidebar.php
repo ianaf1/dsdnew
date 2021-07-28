da55d65d4ec7ee8cfc71bb034b5e476984172a96
ianaf1 <79218845+ianaf1@users.noreply.github.com>
2021-07-28 21:03:56 +0700

    Update v_sidebar.php

--- user/v_sidebar.php
+++ user/v_sidebar.php
@@ -27,17 +27,18 @@
     </div>
 
     <!-- Nav Item - Pages Master Menu -->
+    <div>
+        <li class="dropdown ">
+            <a href="#" class="nav-link has-dropdown"><i class="fas fa-fire fa-fw"></i> <span>Profil</span></a>
+            <ul class="dropdown-menu">
+                <li><a class="nav-link" href="?pg=detail">Lihat Profil</a></li>
+                <li><a class="nav-link" href="?pg=formulir">Edit Profil</a></li>
+            </ul>
+        </li>
 
-    <li class="dropdown ">
-        <a href="#" class="nav-link has-dropdown"><i class="fas fa-fire fa-fw"></i> <span>Profil</span></a>
-        <ul class="dropdown-menu">
-            <li><a class="nav-link" href="?pg=detail">Lihat Profil</a></li>
-            <li><a class="nav-link" href="?pg=formulir">Edit Profil</a></li>
-        </ul>
-    </li>
-
-    <li><a class="nav-link" href="?pg=bayar"><i class="fas fa-money-bill    "></i> <span>Pembayaran</span></a></li>
-    <li><a class="nav-link" href="?pg=pengumuman"><i class="fas fa-bullhorn fa-fw"></i> <span>Pengumuman</span></a></li>
+        <li><a class="nav-link" href="?pg=bayar"><i class="fas fa-money-bill    "></i> <span>Pembayaran</span></a></li>
+        <li><a class="nav-link" href="?pg=pengumuman"><i class="fas fa-bullhorn fa-fw"></i> <span>Pengumuman</span></a></li>
+    </div>
 
 
     <!-- Sidebar Toggler (Sidebar) -->
