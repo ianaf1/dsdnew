6880925d9182a048e7ce639dc0cb5c308a0bc093
ianaf1 <79218845+ianaf1@users.noreply.github.com>
2021-07-30 02:09:35 +0700

    Update export_kelas.php

--- admin/mod_rombel/export_kelas.php
+++ admin/mod_rombel/export_kelas.php
@@ -6,39 +6,38 @@ $id_kelas = dekripsi($_GET['id']);
 $kelas = fetch($koneksi, 'kelas', ['id_kelas' => $id_kelas]);
 // Skrip berikut ini adalah skrip yang bertugas untuk meng-export data tadi ke excell
 header("Content-type: application/vnd-ms-excel");
-header("Content-Disposition: attachment; filename=Data Siswa Kelas $kelas[nama_kelas].xls"); { ?>
+header("Content-Disposition: attachment; filename=Data Siswa Kelas $kelas[nama_kelas].xls");
+?>
 
+<table style="font-size: 12px" class="table table-bordered table-sm" id="table-1">
     <center>
-        <h4>DATA SISWA KELAS <php= $kelas['nama_kelas'] <?php } ?> </h4>
+        <h4>DATA SISWA KELAS</h4>
     </center>
-    <table border="1">
-        <thead>
+    <thead>
+        <tr>
+            <th class="text-center">
+                No
+            </th>
+            <th class="text-center">NIS</th>
+            <th class="text-center">Nama Siswa</th>
+            <th class="text-center">L/P</th>
+        </tr>
+    </thead>
+    <tbody>
+        <?php
+        $query = mysqli_query($koneksi, "select * from daftar a join kelas b ON a.id_kelas=b.id_kelas where a.id_kelas='$kelas[id_kelas]' order by a.nama asc");
+        $no = 0;
+        while ($daftar = mysqli_fetch_array($query)) {
+            $no++;
+        ?>
             <tr>
-                <th class="text-center">
-                    No
-                </th>
-                <th class="text-center">NIS</th>
-                <th class="text-center">Nama Siswa</th>
-                <th class="text-center">L/P</th>
+                <td class="text-center"><?= $no; ?></td>
+                <td class="str"><?= $daftar['nis'] ?></td>
+                <td><?= $daftar['nama'] ?></td>
+                <td class="text-center"><?= $daftar['jenkel'] ?></td>
             </tr>
-        </thead>
-        <tbody>
-            <?php
-            $query = mysqli_query($koneksi, "select * from daftar a join kelas b ON a.id_kelas=b.id_kelas where a.id_kelas='$kelas[id_kelas]' order by a.nama asc");
-            $no = 0;
-            while ($daftar = mysqli_fetch_array($query)) {
-                $no++;
-            ?>
-                <tr>
-                    <td class="text-center"><?= $no; ?></td>
-                    <td class="text-center"><?= $daftar['nis'] ?></td>
-                    <td><?= $daftar['nama'] ?></td>
-                    <td class="text-center"><?= $daftar['jenkel'] ?></td>
-                </tr>
 
-            <?php }
-            ?>
-
-
-        </tbody>
-    </table>
+        <?php }
+        ?>
+    </tbody>
+</table>
