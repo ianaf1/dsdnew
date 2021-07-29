4203972c14754db5bc70d705c6be145460ec95f7
ianaf1 <79218845+ianaf1@users.noreply.github.com>
2021-07-30 01:41:27 +0700

    update download rombel

---
+++ admin/mod_rombel/export_kelas.php
@@ -0,0 +1,47 @@
+<?php
+require "../../config/database.php";
+require "../../config/function.php";
+require "../../config/functions.crud.php";
+$id_kelas = dekripsi($_GET['id']);
+$kelas = fetch($koneksi, 'kelas', ['id_kelas' => $id_kelas]);
+// Skrip berikut ini adalah skrip yang bertugas untuk meng-export data tadi ke excell
+header("Content-type: application/vnd-ms-excel");
+header("Content-Disposition: attachment; filename=datasemua_pembayaran.xls");
+?>
+
+<center>
+    <h3>DATA SISWA KELAS <php= $kelas['nama_kelas'] ?>
+    </h3>
+</center>
+<table border="1">
+    <thead>
+        <tr>
+            <th class="text-center">
+                No
+            </th>
+            <th class="text-center">NIS</th>
+            <th class="text-center">Nama Siswa</th>
+            <th class="text-center">L/P</th>
+        </tr>
+    </thead>
+    <tbody>
+        <?php
+        $query = mysqli_query($koneksi, "select * from daftar a join kelas b ON a.id_kelas=b.id_kelas where a.id_kelas='$kelas[id_kelas]' order by a.nama asc");
+        $no = 0;
+        while ($daftar = mysqli_fetch_array($query)) {
+            $no++;
+        ?>
+            <tr>
+                <td class="text-center"><?= $no; ?></td>
+                <td class="text-center"><?= $daftar['nis'] ?></td>
+                <td><?= $daftar['nama'] ?></td>
+                <td class="text-center"><?= $daftar['jenkel'] ?></td>
+
+            </tr>
+
+        <?php }
+        ?>
+
+
+    </tbody>
+</table>
