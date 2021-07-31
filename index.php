4dca26aaa8240efa1228c57d17b72df3ed22fa96
ianaf1 <79218845+ianaf1@users.noreply.github.com>
2021-07-31 16:29:27 +0700

    Update index.php
    tambah adsense

--- index.php
+++ index.php
@@ -53,6 +53,8 @@ require "config/functions.crud.php";
         action();
     </script>
 
+    <script data-ad-client="ca-pub-8017078778323364" async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+
 </head>
 
 <body>
