26de5dd9a83e7262b3572de944be7b0911102326
ianaf1 <79218845+ianaf1@users.noreply.github.com>
2021-08-02 22:22:26 +0700

    update eksport kas

--- admin/mod_daftar/daftar.php
+++ admin/mod_daftar/daftar.php
@@ -179,7 +179,7 @@ ini_set('display_errors', 1);
                     </tfoot>
                     <tbody>
                         <?php
-                        $query = mysqli_query($koneksi, "select * from daftar a join kelas b ON a.id_kelas=b.id_kelas where a.status='1' order by a.nama && b.nama_kelas");
+                        $query = mysqli_query($koneksi, "select * from daftar a join kelas b ON a.id_kelas=b.id_kelas where a.status='1' order by a.nama && b.nama_kelas asc");
                         $no = 0;
                         while ($daftar = mysqli_fetch_array($query)) {
                             $no++;
