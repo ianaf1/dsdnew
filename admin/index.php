f662c13feb1a95bf3e0ce51151fe743b6b8baac5
ianaf1 <79218845+ianaf1@users.noreply.github.com>
2021-08-11 13:38:14 +0700

    update lagi

--- admin/index.php
+++ admin/index.php
@@ -4,6 +4,31 @@ require("../config/database.php");
 require("../config/function.php");
 require("../config/functions.crud.php");
 
+$jan = mysqli_fetch_array(mysqli_query($koneksi, "select sum(debit) as totaldebit, sum(kredit) as totalkredit from transaksi where id_bulan=1"));
+$feb = mysqli_fetch_array(mysqli_query($koneksi, "select sum(debit) as totaldebit, sum(kredit) as totalkredit from transaksi where id_bulan=2"));
+$mar = mysqli_fetch_array(mysqli_query($koneksi, "select sum(debit) as totaldebit, sum(kredit) as totalkredit from transaksi where id_bulan=3"));
+$apr = mysqli_fetch_array(mysqli_query($koneksi, "select sum(debit) as totaldebit, sum(kredit) as totalkredit from transaksi where id_bulan=4"));
+$mei = mysqli_fetch_array(mysqli_query($koneksi, "select sum(debit) as totaldebit, sum(kredit) as totalkredit from transaksi where id_bulan=5"));
+$jun = mysqli_fetch_array(mysqli_query($koneksi, "select sum(debit) as totaldebit, sum(kredit) as totalkredit from transaksi where id_bulan=6"));
+$jul = mysqli_fetch_array(mysqli_query($koneksi, "select sum(debit) as totaldebit, sum(kredit) as totalkredit from transaksi where id_bulan=7"));
+$agu = mysqli_fetch_array(mysqli_query($koneksi, "select sum(debit) as totaldebit, sum(kredit) as totalkredit from transaksi where id_bulan=8"));
+$sep = mysqli_fetch_array(mysqli_query($koneksi, "select sum(debit) as totaldebit, sum(kredit) as totalkredit from transaksi where id_bulan=9"));
+$okt = mysqli_fetch_array(mysqli_query($koneksi, "select sum(debit) as totaldebit, sum(kredit) as totalkredit from transaksi where id_bulan=10"));
+$nov = mysqli_fetch_array(mysqli_query($koneksi, "select sum(debit) as totaldebit, sum(kredit) as totalkredit from transaksi where id_bulan=11"));
+$des = mysqli_fetch_array(mysqli_query($koneksi, "select sum(debit) as totaldebit, sum(kredit) as totalkredit from transaksi where id_bulan=12"));
+$saldojan = $jan['totaldebit'] - $jan['totalkredit'];
+$saldofeb = $feb['totaldebit'] - $feb['totalkredit'];
+$saldomar = $mar['totaldebit'] - $mar['totalkredit'];
+$saldoapr = $apr['totaldebit'] - $apr['totalkredit'];
+$saldomei = $mei['totaldebit'] - $mei['totalkredit'];
+$saldojun = $jun['totaldebit'] - $jun['totalkredit'];
+$saldojul = $jul['totaldebit'] - $jul['totalkredit'];
+$saldoagu = $agu['totaldebit'] - $agu['totalkredit'];
+$saldosep = $sep['totaldebit'] - $sep['totalkredit'];
+$saldookt = $okt['totaldebit'] - $okt['totalkredit'];
+$saldonov = $nov['totaldebit'] - $nov['totalkredit'];
+$saldodes = $des['totaldebit'] - $des['totalkredit'];
+
 if (isset($_SESSION['id_user'])) {
   $user = mysqli_fetch_array(mysqli_query($koneksi, "select * from user where id_user='$_SESSION[id_user]'"));
 ?>
@@ -179,6 +204,174 @@ if (isset($_SESSION['id_user'])) {
       }).closest('.treeview').addClass('active');
     </script>
   </body>
+  <script>
+    var ctx = document.getElementById("myChartSiswa");
+    var myChartSiswa = new Chart(ctx, {
+      type: 'doughnut',
+      data: {
+        labels: ["Kelas 10", "Kelas 11", "Kelas 12"],
+        datasets: [{
+          data: [
+            <?= mysqli_num_rows(mysqli_query($koneksi, "select * from daftar where kelas = '10' && status='1'")) ?>,
+            <?= mysqli_num_rows(mysqli_query($koneksi, "select * from daftar where kelas = '11' && status='1'")) ?>,
+            <?= mysqli_num_rows(mysqli_query($koneksi, "select * from daftar where kelas = '12' && status='1'")) ?>
+          ],
+          backgroundColor: ['#4e73df', '#1cc88a', '#36b9cc'],
+          hoverBackgroundColor: ['#2e59d9', '#17a673', '#2c9faf'],
+          hoverBorderColor: "rgba(234, 236, 244, 1)",
+        }],
+      },
+      options: {
+        maintainAspectRatio: false,
+        tooltips: {
+          backgroundColor: "rgb(255,255,255)",
+          bodyFontColor: "#858796",
+          borderColor: '#dddfeb',
+          borderWidth: 1,
+          xPadding: 15,
+          yPadding: 15,
+          displayColors: false,
+          caretPadding: 10,
+        },
+        legend: {
+          display: false
+        },
+        cutoutPercentage: 80,
+      },
+    });
+  </script>
+  <script>
+    Chart.defaults.global.defaultFontFamily = 'Nunito', '-apple-system,system-ui,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,sans-serif';
+    Chart.defaults.global.defaultFontColor = '#858796';
+
+    function number_format(number, decimals, dec_point, thousands_sep) {
+      // *     example: number_format(1234.56, 2, ',', ' ');
+      // *     return: '1 234,56'
+      number = (number + '').replace(',', '').replace(' ', '');
+      var n = !isFinite(+number) ? 0 : +number,
+        prec = !isFinite(+decimals) ? 0 : Math.abs(decimals),
+        sep = (typeof thousands_sep === 'undefined') ? ',' : thousands_sep,
+        dec = (typeof dec_point === 'undefined') ? '.' : dec_point,
+        s = '',
+        toFixedFix = function(n, prec) {
+          var k = Math.pow(10, prec);
+          return '' + Math.round(n * k) / k;
+        };
+      // Fix for IE parseFloat(0.55).toFixed(0) = 0;
+      s = (prec ? toFixedFix(n, prec) : '' + Math.round(n)).split('.');
+      if (s[0].length > 3) {
+        s[0] = s[0].replace(/\B(?=(?:\d{3})+(?!\d))/g, sep);
+      }
+      if ((s[1] || '').length < prec) {
+        s[1] = s[1] || '';
+        s[1] += new Array(prec - s[1].length + 1).join('0');
+      }
+      return s.join(dec);
+    }
+
+    // Area Chart Example
+    var ctx = document.getElementById("kasKeluar");
+    var myLineChart = new Chart(ctx, {
+      type: 'line',
+      data: {
+        labels: ["Jan", "Feb", "Mar", "Apr", "Mei", "Jun", "Jul", "Agu", "Sep", "Okt", "Nov", "Des"],
+        datasets: [{
+          label: "Earnings",
+          lineTension: 0.3,
+          backgroundColor: "rgba(78, 115, 223, 0.05)",
+          borderColor: "rgba(78, 115, 223, 1)",
+          pointRadius: 3,
+          pointBackgroundColor: "rgba(78, 115, 223, 1)",
+          pointBorderColor: "rgba(78, 115, 223, 1)",
+          pointHoverRadius: 3,
+          pointHoverBackgroundColor: "rgba(78, 115, 223, 1)",
+          pointHoverBorderColor: "rgba(78, 115, 223, 1)",
+          pointHitRadius: 10,
+          pointBorderWidth: 2,
+          data: [
+            <?= $saldojan ?>,
+            <?= $saldofeb ?>,
+            <?= $saldomar ?>,
+            <?= $saldoapr ?>,
+            <?= $saldomei ?>,
+            <?= $saldojun ?>,
+            <?= $saldojul ?>,
+            <?= $saldoagu ?>,
+            <?= $saldosep ?>,
+            <?= $saldookt ?>,
+            <?= $saldonov ?>,
+            <?= $saldodes ?>,
+          ],
+        }],
+      },
+      options: {
+        maintainAspectRatio: false,
+        layout: {
+          padding: {
+            left: 10,
+            right: 25,
+            top: 25,
+            bottom: 0
+          }
+        },
+        scales: {
+          xAxes: [{
+            time: {
+              unit: 'date'
+            },
+            gridLines: {
+              display: false,
+              drawBorder: false
+            },
+            ticks: {
+              maxTicksLimit: 7
+            }
+          }],
+          yAxes: [{
+            ticks: {
+              maxTicksLimit: 5,
+              padding: 10,
+              // Include a dollar sign in the ticks
+              callback: function(value, index, values) {
+                return 'Rp.' + number_format(value);
+              }
+            },
+            gridLines: {
+              color: "rgb(234, 236, 244)",
+              zeroLineColor: "rgb(234, 236, 244)",
+              drawBorder: false,
+              borderDash: [2],
+              zeroLineBorderDash: [2]
+            }
+          }],
+        },
+        legend: {
+          display: false
+        },
+        tooltips: {
+          backgroundColor: "rgb(255,255,255)",
+          bodyFontColor: "#858796",
+          titleMarginBottom: 10,
+          titleFontColor: '#6e707e',
+          titleFontSize: 14,
+          borderColor: '#dddfeb',
+          borderWidth: 1,
+          xPadding: 15,
+          yPadding: 15,
+          displayColors: false,
+          intersect: false,
+          mode: 'index',
+          caretPadding: 10,
+          callbacks: {
+            label: function(tooltipItem, chart) {
+              var datasetLabel = chart.datasets[tooltipItem.datasetIndex].label || '';
+              return datasetLabel + ': Rp.' + number_format(tooltipItem.yLabel);
+            }
+          }
+        }
+      }
+    });
+  </script>
 
   </html>
 <?php
