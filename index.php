2f52b6566a5693c5579d0dc8225b3d9441c62fac
ianaf1 <ianfatah1@gmail.com>
2021-08-11 23:46:18 +0700

    Update index.php

--- index.php
+++ index.php
@@ -111,16 +111,49 @@ require "config/functions.crud.php";
                         <div class="col-lg-6 align-self-center">
                             <div class="left-content header-text wow fadeInLeft" data-wow-duration="1s" data-wow-delay="1s">
                                 <div class="row">
-                                    <div class="col-lg-4 col-sm-4">
-                                        <div class="info-stat">
-                                            <h6>Jumlah Guru:</h6>
-                                            <h4><?= mysqli_num_rows(mysqli_query($koneksi, "select * from ptk")) ?></h4>
+                                    <div class="col-xl-3 col-md-6 mb-4">
+                                        <div class="card border-left-primary shadow h-100 py-2">
+                                            <div class="card-body">
+                                                <div class="row no-gutters align-items-center">
+                                                    <div class="col mr-2">
+                                                        <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">Jumlah Siswa</div>
+                                                        <div class="h5 mb-0 font-weight-bold text-gray-800"><?= rowcount($koneksi, 'daftar', ['status' => 1]) ?></div>
+                                                    </div>
+                                                    <div class="col-auto">
+                                                        <i class="fas fa-user fa-2x text-gray-300"></i>
+                                                    </div>
+                                                </div>
+                                            </div>
                                         </div>
                                     </div>
-                                    <div class="col-lg-4 col-sm-4">
-                                        <div class="info-stat">
-                                            <h6>Jumlah Siswa:</h6>
-                                            <h4><?= mysqli_num_rows(mysqli_query($koneksi, "select * from daftar where status = '1'")) ?></h4>
+                                    <div class="col-xl-3 col-md-6 mb-4">
+                                        <div class="card border-left-success shadow h-100 py-2">
+                                            <div class="card-body">
+                                                <div class="row no-gutters align-items-center">
+                                                    <div class="col mr-2">
+                                                        <div class="text-xs font-weight-bold text-success text-uppercase mb-1">Laki Laki</div>
+                                                        <div class="h5 mb-0 font-weight-bold text-gray-800"><?= mysqli_num_rows(mysqli_query($koneksi, "select * from daftar where jenkel = 'L' && status = '1'")) ?></div>
+                                                    </div>
+                                                    <div class="col-auto">
+                                                        <i class="fas fas fa-user fa-2x text-gray-300"></i>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="col-xl-3 col-md-6 mb-4">
+                                        <div class="card border-left-success shadow h-100 py-2">
+                                            <div class="card-body">
+                                                <div class="row no-gutters align-items-center">
+                                                    <div class="col mr-2">
+                                                        <div class="text-xs font-weight-bold text-success text-uppercase mb-1">Perempuan</div>
+                                                        <div class="h5 mb-0 font-weight-bold text-gray-800"><?= mysqli_num_rows(mysqli_query($koneksi, "select * from daftar where jenkel = 'P' && status = '1'")) ?></div>
+                                                    </div>
+                                                    <div class="col-auto">
+                                                        <i class="fas fas fa-user fa-2x text-gray-300"></i>
+                                                    </div>
+                                                </div>
+                                            </div>
                                         </div>
                                     </div>
                                     <div class="col-lg-12">
