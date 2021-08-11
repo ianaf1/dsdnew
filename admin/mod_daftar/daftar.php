f3995fc6dbdf9e59cf208005f98302ba7193f72e
ianaf1 <79218845+ianaf1@users.noreply.github.com>
2021-08-12 04:30:32 +0700

    balik lagi

--- admin/mod_daftar/daftar.php
+++ admin/mod_daftar/daftar.php
@@ -141,7 +141,7 @@ ini_set('display_errors', 1);
         <div class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
             <h5 class="m-0 font-weight-bold text-primary">Siswa Aktif</h5>
             <div class="dropdown no-arrow">
-                <a class="dropdown-toggle" role="button" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                <a class="dropdown-toggle" href="#" role="button" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                     <i class="fas fa-ellipsis-v fa-sm fa-fw text-gray-400"></i>
                 </a>
                 <div class="dropdown-menu dropdown-menu-right shadow animated--fade-in" aria-labelledby="dropdownMenuLink">
