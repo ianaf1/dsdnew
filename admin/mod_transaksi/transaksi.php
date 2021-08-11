2a9c164ffe9fb4f62c0e285e95c2fac05f0074ef
ianaf1 <79218845+ianaf1@users.noreply.github.com>
2021-08-12 04:26:04 +0700

    update

--- admin/mod_transaksi/transaksi.php
+++ admin/mod_transaksi/transaksi.php
@@ -167,7 +167,7 @@ $saldoawal = $saldolama;
                 }
                 ?>
                 <div class="dropdown no-arrow">
-                    <a class="dropdown-toggle" href="#" role="button" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    <a class="dropdown-toggle" role="button" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                         <i class="fas fa-ellipsis-v fa-sm fa-fw text-gray-400"></i>
                     </a>
                     <div class="dropdown-menu dropdown-menu-right shadow animated--fade-in" aria-labelledby="dropdownMenuLink">
