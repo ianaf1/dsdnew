ef179a832acba1f33a1be1107e0f54982a6fe4a9
ianaf1 <79218845+ianaf1@users.noreply.github.com>
2021-08-13 21:39:14 +0700

    Update index.php

--- index.php
+++ index.php
@@ -188,9 +188,9 @@ require "config/functions.crud.php";
     <div id="tutor" class="contact-us section">
         <div class="container">
             <div class="row">
-                <div class="col-lg-14">
+                <div class="col-lg-12">
                     <div class="row">
-                        <div class="col-lg-14 align-self-center wow fadeInLeft" data-wow-duration="1s" data-wow-delay="0.5s">
+                        <div class="col-lg-6 align-self-center wow fadeInLeft" data-wow-duration="1s" data-wow-delay="0.5s">
                             <div class="card shadow mb-4">
                                 <div class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
                                     <h7 class="m-0 font-weight-bold text-primary">Tutorial Pengisian</h7>
@@ -252,7 +252,7 @@ require "config/functions.crud.php";
                                 </div>
                             </div>
                         </div>
-                        <div class="col-lg-14 align-self-center">
+                        <div class="col-lg-6 align-self-center">
                             <div class="right-image wow fadeInRight" data-wow-duration="1s" data-wow-delay="0.5s">
                                 <div class="card shadow mb-4">
                                     <div class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
