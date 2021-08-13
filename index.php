b955c870fd7e7a35d215a958827a89c47a957c31
ianaf1 <79218845+ianaf1@users.noreply.github.com>
2021-08-13 19:39:23 +0700

    Update index.php

--- index.php
+++ index.php
@@ -91,9 +91,6 @@ require "config/functions.crud.php";
                         <div class="col-lg-6 align-self-center">
                             <div class="left-content wow fadeInLeft" data-wow-duration="1s" data-wow-delay="1s">
                                 <div class="row">
-                                    <div class="col-lg-12">
-                                        <h2>DATA SEKOLAH DIGITAL</h2>
-                                    </div>
                                     <div class="col-lg-4 col-sm-4">
                                         <div class="card border-left-primary shadow h-100 py-2">
                                             <div class="card-body">
@@ -129,6 +126,9 @@ require "config/functions.crud.php";
                                                 </div>
                                             </div>
                                         </div>
+                                    </div>
+                                    <div class="col-lg-12">
+                                        <h2>DATA SEKOLAH DIGITAL</h2>
                                     </div>
                                 </div>
                             </div>
