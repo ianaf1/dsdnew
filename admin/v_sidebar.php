9ee36856dbbd43b02d9af57cf953687bfdef1838
ianaf1 <79218845+ianaf1@users.noreply.github.com>
2021-08-16 12:51:16 +0700

    Update v_sidebar.php

--- admin/v_sidebar.php
+++ admin/v_sidebar.php
@@ -64,9 +64,9 @@
                     <h6 class="collapse-header">Data Siswa</h6>
                     <a class="collapse-item" href="?pg=daftar">Siswa Aktif</a>
                     <a class="collapse-item" href="?pg=mutasi">Siswa Mutasi</a>
-                    <a class="collapse-item" href="?pg=kelas10">Kelas 10</a>
+                    <!-- <a class="collapse-item" href="?pg=kelas10">Kelas 10</a>
                     <a class="collapse-item" href="?pg=kelas11">Kelas 11</a>
-                    <a class="collapse-item" href="?pg=kelas12">Kelas 12</a>
+                    <a class="collapse-item" href="?pg=kelas12">Kelas 12</a> -->
                 </div>
             </div>
         </li>
