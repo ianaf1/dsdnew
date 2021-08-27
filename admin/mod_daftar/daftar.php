cdbe3a159067ff2a29da1c84cdf8e8f1148c2c19
ianaf1 <79218845+ianaf1@users.noreply.github.com>
2021-08-27 11:41:02 +0700

    Update daftar.php

--- admin/mod_daftar/daftar.php
+++ admin/mod_daftar/daftar.php
@@ -226,10 +226,10 @@ ini_set('display_errors', 1);
                     <tbody>
                         <?php
                         if (isset($_GET['id']) == '') {
-                            $query = mysqli_query($koneksi, "select * from daftar where status='1' order by nama asc");
+                            $query = mysqli_query($koneksi, "select * from daftar order by nama asc");
                         } else {
                             $kelas = $_GET['id'];
-                            $query = mysqli_query($koneksi, "select * from daftar a join kelas b ON a.id_kelas=b.id_kelas where a.kelas='$kelas' && a.status='1' order by b.nama_kelas asc");
+                            $query = mysqli_query($koneksi, "select * from daftar a join kelas b ON a.id_kelas=b.id_kelas where a.kelas='$kelas' order by b.nama_kelas asc");
                         }
                         $no = 0;
                         while ($daftar = mysqli_fetch_array($query)) {
