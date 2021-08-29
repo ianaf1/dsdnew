c896e240700a07c3c3a74f5ef80d07b232f4d219
ianaf1 <ianfatah1@gmail.com>
2021-08-29 20:14:19 +0700

    Update bayar.php

--- admin/mod_bayar/bayar.php
+++ admin/mod_bayar/bayar.php
@@ -50,10 +50,11 @@
                                 <tbody>
                                     <?php
                                     $query = mysqli_query($koneksi, "select id_daftar, nama, kelas from daftar where status='1'");
-                                    $q2 = mysqli_query($koneksi, "select sum(jumlah) as total from biaya where id_kelas='$daftar[kelas]' && id_semester != '$semester_aktif[id_semester]' || thn_ajaran!='$tahun_ajaran_aktif[nama_thn_ajaran]'");
-                                    $q3 = mysqli_query($koneksi, "select sum(jumlah) as total from siswa_tunggakan where id_daftar='$daftar[id_daftar]'");
-                                    $q4 = mysqli_query($koneksi, "select sum(jumlah) as total from bayar where id_daftar='$daftar[id_daftar]' && id_biaya='L'");
+
                                     while ($daftar = mysqli_fetch_array($query)) {
+                                        $q2 = mysqli_query($koneksi, "select sum(jumlah) as total from biaya where id_kelas='$daftar[kelas]' && id_semester != '$semester_aktif[id_semester]' || thn_ajaran!='$tahun_ajaran_aktif[nama_thn_ajaran]'");
+                                        $q3 = mysqli_query($koneksi, "select sum(jumlah) as total from siswa_tunggakan where id_daftar='$daftar[id_daftar]'");
+                                        $q4 = mysqli_query($koneksi, "select sum(jumlah) as total from bayar where id_daftar='$daftar[id_daftar]' && id_biaya='L'");
                                         $tunggakansiswa = mysqli_fetch_array($q3);
                                         $biayasiswa = mysqli_fetch_array($q2);
                                         $siswabayar = mysqli_fetch_array($q4);
