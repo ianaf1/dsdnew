2bb029d7943417a666e409c11ba9ac89f19eed35
ianaf1 <ianfatah1@gmail.com>
2021-09-12 03:33:46 +0700

    Update bayar.php

--- user/mod_bayar/bayar.php
+++ user/mod_bayar/bayar.php
@@ -133,7 +133,7 @@
                         </thead>
                         <tbody>
                             <?php
-                            $query = mysqli_query($koneksi, "select * from tunggakan where a.id_daftar='$siswa[id_daftar]'");
+                            $query = mysqli_query($koneksi, "select * from tunggakan where id_daftar='$siswa[id_daftar]'");
                             $no = 0;
                             while ($tunggakan = mysqli_fetch_array($query)) {
                                 $no++;
