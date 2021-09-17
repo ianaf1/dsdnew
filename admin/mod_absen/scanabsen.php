4af6d15e176827e911516b8a3ded8b6df0e29be2
ianaf1 <ianfatah1@gmail.com>
2021-09-18 02:36:33 +0700

    Update scanabsen.php

--- admin/mod_absen/scanabsen.php
+++ admin/mod_absen/scanabsen.php
@@ -48,8 +48,9 @@
         let audio = new Audio("../../assets/audio/beep.mp3");
         const codeReader = new ZXing.BrowserQRCodeReader()
         console.log('ZXing code reader initialized')
-        codeReader.getVideoInputDevices()
-            .then((videoInputDevices) => {
+        // codeReader.getVideoInputDevices()
+        //     .then((videoInputDevices) => {
+        codeReader.decodeFromVideoDevice(undefined, 'video', (result, err) => {
                 const sourceSelect = document.getElementById('sourceSelect')
                 selectedDeviceId = videoInputDevices[0].deviceId
                 if (videoInputDevices.length >= 1) {
