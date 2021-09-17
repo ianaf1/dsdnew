6141680911c8287b01a9f15d467aff40b3559fac
ianaf1 <ianfatah1@gmail.com>
2021-09-17 08:53:57 +0700

    update lagi

--- admin/mod_absen/scanabsen.php
+++ admin/mod_absen/scanabsen.php
@@ -1,6 +1,6 @@
 <?php defined('BASEPATH') or die("ip anda sudah tercatat oleh sistem kami") ?>
 
-<link href="../assets/webcodecam/css/bootstrap.min.css" rel="stylesheet">
+<!-- <link href="../assets/webcodecam/css/bootstrap.min.css" rel="stylesheet"> -->
 <link href="../assets/webcodecam/css/style.css" rel="stylesheet">
 <div class="row">
     <div class="col-12">
@@ -10,14 +10,14 @@
                     <h5 class="m-0 font-weight-bold text-primary">Scan Absensi</h5>
                     <div class="card-header-action">
                         <select class="form-control" id="camera-select"></select>
-                        <div class="form-group">
+                        <!-- <div class="form-group">
                             <input id="image-url" type="text" class="form-control" placeholder="Image url">
                             <button title="Decode Image" class="btn btn-default btn-sm" id="decode-img" type="button" data-toggle="tooltip"><span class="glyphicon glyphicon-upload"></span></button>
                             <button title="Image shoot" class="btn btn-info btn-sm disabled" id="grab-img" type="button" data-toggle="tooltip"><span class="glyphicon glyphicon-picture"></span></button>
                             <button title="Play" class="btn btn-success btn-sm" id="play" type="button" data-toggle="tooltip"><span class="glyphicon glyphicon-play"></span></button>
                             <button title="Pause" class="btn btn-warning btn-sm" id="pause" type="button" data-toggle="tooltip"><span class="glyphicon glyphicon-pause"></span></button>
                             <button title="Stop streams" class="btn btn-danger btn-sm" id="stop" type="button" data-toggle="tooltip"><span class="glyphicon glyphicon-stop"></span></button>
-                        </div>
+                        </div> -->
                     </div>
                 </div>
                 <div class="card-body">
@@ -32,9 +32,9 @@
                     </div>
                     <div class="col-md-6">
                         <div class="thumbnail" id="result">
-                            <div class="well" style="overflow: hidden;">
+                            <!-- <div class="well" style="overflow: hidden;">
                                 <img width="320" height="240" id="scanned-img" src="">
-                            </div>
+                            </div> -->
                             <div class="caption">
                                 <h3>Scanned result</h3>
                                 <p id="scanned-QR"></p>
