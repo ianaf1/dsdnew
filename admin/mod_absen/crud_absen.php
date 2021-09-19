b2e0347031c45718c5f98c47b763b2d4da718bb2
ianaf1 <ianfatah1@gmail.com>
2021-09-19 16:57:52 +0700

    Update crud_absen.php

--- admin/mod_absen/crud_absen.php
+++ admin/mod_absen/crud_absen.php
@@ -6,11 +6,11 @@ require("../../config/tahun.ajaran.php");
 // session_start();
 if ($pg == 'presen') {
     $tgl = date('Ymd');
-    $jam = date('h:i:s');
+    $jam = date('H:i:s');
     $nis = mysqli_escape_string($koneksi, $_POST['nis']);
     $siswaQ = mysqli_query($koneksi, "SELECT * FROM daftar WHERE nis='$nis'");
     $siswaR = mysqli_fetch_array($siswaQ);
-    $presensiQ = mysqli_query($koneksi, "SELECT * FROM daftar WHERE nis='$nis' AND tgl='$tgl'");
+    $presensiQ = mysqli_query($koneksi, "SELECT * FROM presensi WHERE nis='$nis' AND tgl='$tgl'");
     $presensiR = mysqli_fetch_array($presensiQ);
     if (mysqli_num_rows($siswaQ) == 1) {
         if (mysqli_num_rows($presensiQ) < 1) {
@@ -19,6 +19,12 @@ if ($pg == 'presen') {
                 'tgl' => $tgl
             ];
             insert($koneksi, 'presensi', $data);
+        } else {
+            $data = [
+                'jam_plg' => $jam,
+                'ket' => 'Hadir'
+            ];
+            update($koneksi, 'presensi', $data, ['nis' => $_POST['nis']]);
         }
     }
 }
