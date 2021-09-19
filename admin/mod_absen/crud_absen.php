c7bc1351eef222aa80dfb8904f365ee2a96f41e5
ianaf1 <ianfatah1@gmail.com>
2021-09-19 20:17:17 +0700

    Update crud_absen.php

--- admin/mod_absen/crud_absen.php
+++ admin/mod_absen/crud_absen.php
@@ -7,13 +7,15 @@ require("../../config/tahun.ajaran.php");
 if ($pg == 'presen') {
     $tgl = date('Ymd');
     $jam = date('H:i:s');
+    $jam_msk = '20:30:00';
+    $jam_plg = '21:30:00';
     $nis = mysqli_escape_string($koneksi, $_POST['nis']);
     $siswaQ = mysqli_query($koneksi, "SELECT * FROM daftar WHERE nis='$nis'");
     $siswaR = mysqli_fetch_array($siswaQ);
     $presensiQ = mysqli_query($koneksi, "SELECT * FROM presensi WHERE nis='$siswaR[nis]' AND tgl='$tgl'");
     $presensiR = mysqli_fetch_array($presensiQ);
     if (mysqli_num_rows($siswaQ) == 1) {
-        if (mysqli_num_rows($presensiQ) == 0 && $jam < '20:30:00') {
+        if (mysqli_num_rows($presensiQ) == 0 && $jam < $jam_msk) {
             $data = [
                 'nis' => $_POST['nis'],
                 'tgl' => $tgl
@@ -23,12 +25,12 @@ if ($pg == 'presen') {
             ];
             echo json_encode($pesan);
             insert($koneksi, 'presensi', $data);
-        } elseif (mysqli_num_rows($presensiQ) == 0 && $jam > '20:30:00') {
+        } elseif (mysqli_num_rows($presensiQ) == 0 && $jam > $jam_msk) {
             $pesan = [
                 'pesan' => 'ggl_masuk'
             ];
             echo json_encode($pesan);
-        } elseif (mysqli_num_rows($presensiQ) == 1 && $jam > '21:00:00') {
+        } elseif (mysqli_num_rows($presensiQ) == 1 && $jam > $jam_plg) {
             $data = [
                 'jam_plg' => $jam,
                 'ket' => 'Hadir'
@@ -38,12 +40,12 @@ if ($pg == 'presen') {
             ];
             echo json_encode($pesan);
             update($koneksi, 'presensi', $data, ['nis' => $_POST['nis']]);
-        } elseif (mysqli_num_rows($presensiQ) == 1 && $presensiR['jam_msk'] > '00:00:00' && $jam > '20:30:00' && $jam < '21:00:00') {
+        } elseif (mysqli_num_rows($presensiQ) == 1 && $presensiR['jam_msk'] > '00:00:00' && $jam > $jam_msk && $jam < $jam_plg) {
             $pesan = [
                 'pesan' => 'ggl_pulang'
             ];
             echo json_encode($pesan);
-        } elseif (mysqli_num_rows($presensiQ) == 1 && $presensiR['jam_msk'] > '00:00:00' && $jam < '20:30:00') {
+        } elseif (mysqli_num_rows($presensiQ) == 1 && $presensiR['jam_msk'] > '00:00:00' && $jam < $jam_msk) {
             $pesan = [
                 'pesan' => 'sudah_absen'
             ];
