c8dfba54e6b090e893c6fbcda3391b4a6c6e999b
ianaf1 <ianfatah1@gmail.com>
2021-09-24 01:04:08 +0700

    Update presensi_kelas.php

--- admin/mod_presensi/presensi_kelas.php
+++ admin/mod_presensi/presensi_kelas.php
@@ -27,62 +27,7 @@
         </div>
     </form>
 </div>
-<?php if (isset($_GET['tgl']) && isset($_GET['kelas']) == '') { ?>
-    <div class="row">
-        <div class="col-12 col-sm-12 col-lg-12">
-            <div class="card shadow mb-4">
-                <div class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
-                    <h5 class='m-0 font-weight-bold text-primary'>Presensi Siswa Hari <?= hari_ini() ?></h5>
-                    <div class="card-header-action">
-                    </div>
-                </div>
-                <div class="card-body">
-                    <div class="table-responsive">
-                        <table class="table table-striped table-sm" id="table-2" style="font-size: 12px">
-                            <thead>
-                                <tr>
-                                    <th class="text-center">
-                                        #
-                                    </th>
-                                    <th>Nama Siswa</th>
-                                    <th>Kelas</th>
-                                    <!-- <th>Tanggal</th> -->
-                                    <th>Jam Masuk</th>
-                                    <th>Jam Pulang</th>
-                                    <th>Ket</th>
-                                    <!-- <th>Action</th> -->
-                                </tr>
-                            </thead>
-                            <tbody>
-                                <?php
-                                $sekarang = date('Ymd');
-                                $query = mysqli_query($koneksi, "select * from presensi a join kelas b ON a.id_kelas=b.id_kelas where a.tgl='$sekarang'");
-                                $no = 0;
-                                while ($presensi = mysqli_fetch_array($query)) {
-                                    $no++;
-                                ?>
-                                    <tr>
-                                        <td><?= $no; ?></td>
-                                        <td><?= $presensi['nama'] ?></td>
-                                        <td><?= $presensi['nama_kelas'] ?></td>
-                                        <!-- <td><?= $presensi['tgl'] ?></td> -->
-                                        <td><?= $presensi['jam_msk'] ?></td>
-                                        <td><?= $presensi['jam_plg'] ?></td>
-                                        <td><?= $presensi['ket'] ?></td>
-                                        <!-- <td>
-                                        <button data-id="<?= $presensi['id_presensi'] ?>" class="edit btn btn-danger btn-sm"><i class="fas fa-trash    "></i></button>
-                                    </td> -->
-                                    </tr>
-                                <?php }
-                                ?>
-                            </tbody>
-                        </table>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-<?php } else { ?>
+<?php if (isset($_GET['tgl']) && isset($_GET['kelas'])) { ?>
     <?php
     $tgl = fetch($koneksi, 'presensi', ['tgl' => $_GET['tgl']]);
     $kelas = fetch($koneksi, 'presensi', ['id_kelas' => $_GET['kelas']]); { ?>
@@ -141,4 +86,59 @@
             </div>
         </div>
     <?php } ?>
+<?php } else { ?>
+    <div class="row">
+        <div class="col-12 col-sm-12 col-lg-12">
+            <div class="card shadow mb-4">
+                <div class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
+                    <h5 class='m-0 font-weight-bold text-primary'>Presensi Siswa Hari <?= hari_ini() ?></h5>
+                    <div class="card-header-action">
+                    </div>
+                </div>
+                <div class="card-body">
+                    <div class="table-responsive">
+                        <table class="table table-striped table-sm" id="table-2" style="font-size: 12px">
+                            <thead>
+                                <tr>
+                                    <th class="text-center">
+                                        #
+                                    </th>
+                                    <th>Nama Siswa</th>
+                                    <th>Kelas</th>
+                                    <!-- <th>Tanggal</th> -->
+                                    <th>Jam Masuk</th>
+                                    <th>Jam Pulang</th>
+                                    <th>Ket</th>
+                                    <!-- <th>Action</th> -->
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <?php
+                                $sekarang = date('Ymd');
+                                $query = mysqli_query($koneksi, "select * from presensi a join kelas b ON a.id_kelas=b.id_kelas where a.tgl='$sekarang'");
+                                $no = 0;
+                                while ($presensi = mysqli_fetch_array($query)) {
+                                    $no++;
+                                ?>
+                                    <tr>
+                                        <td><?= $no; ?></td>
+                                        <td><?= $presensi['nama'] ?></td>
+                                        <td><?= $presensi['nama_kelas'] ?></td>
+                                        <!-- <td><?= $presensi['tgl'] ?></td> -->
+                                        <td><?= $presensi['jam_msk'] ?></td>
+                                        <td><?= $presensi['jam_plg'] ?></td>
+                                        <td><?= $presensi['ket'] ?></td>
+                                        <!-- <td>
+                                        <button data-id="<?= $presensi['id_presensi'] ?>" class="edit btn btn-danger btn-sm"><i class="fas fa-trash    "></i></button>
+                                    </td> -->
+                                    </tr>
+                                <?php }
+                                ?>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 <?php } ?>
