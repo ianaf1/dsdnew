d3c5e14c89b4a084e6b3fdec00444132c3e7f0e3
ianaf1 <ianfatah1@gmail.com>
2021-09-23 10:35:07 +0700

    Update presensi_kelas.php

--- admin/mod_presensi/presensi_kelas.php
+++ admin/mod_presensi/presensi_kelas.php
@@ -5,15 +5,7 @@
         <div class="form-row">
             <div class="col-md-6 col-xs-6">
                 <div class="form-group">
-                    <select class="form-control select2" style="width: 100%" name="hari" required>
-                        <option value="">Pilih Hari</option>
-                        <?php
-                        $query = mysqli_query($koneksi, "select * from hari");
-                        while ($hari = mysqli_fetch_array($query)) {
-                        ?>
-                            <option value="<?= enkripsi($hari['id_hari']) ?>"><?= $hari['nama_hari'] ?></option>
-                        <?php } ?>
-                    </select>
+                    <input type="date" name="tgl" class="form-control datepicker" value="">
                 </div>
             </div>
             <div class="col-md-6 col-xs-6">
@@ -35,57 +27,118 @@
         </div>
     </form>
 </div>
-<div class="row">
-    <div class="col-12 col-sm-12 col-lg-12">
-        <div class="card shadow mb-4">
-            <div class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
-                <h5 class='m-0 font-weight-bold text-primary'>Presensi Siswa Hari <?= hari_ini() ?></h5>
-                <div class="card-header-action">
+<?php if (isset($_GET['tgl']) && isset($_GET['kelas']) == '') { ?>
+    <div class="row">
+        <div class="col-12 col-sm-12 col-lg-12">
+            <div class="card shadow mb-4">
+                <div class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
+                    <h5 class='m-0 font-weight-bold text-primary'>Presensi Siswa Hari <?= hari_ini() ?></h5>
+                    <div class="card-header-action">
+                    </div>
                 </div>
-            </div>
-            <div class="card-body">
-                <div class="table-responsive">
-                    <table class="table table-striped table-sm" id="table-2" style="font-size: 12px">
-                        <thead>
-                            <tr>
-                                <th class="text-center">
-                                    #
-                                </th>
-                                <th>Nama Siswa</th>
-                                <th>Kelas</th>
-                                <!-- <th>Tanggal</th> -->
-                                <th>Jam Masuk</th>
-                                <th>Jam Pulang</th>
-                                <th>Ket</th>
-                                <!-- <th>Action</th> -->
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <?php
-                            $sekarang = date('Ymd');
-                            $query = mysqli_query($koneksi, "select * from presensi a join kelas b ON a.id_kelas=b.id_kelas where a.tgl='$sekarang'");
-                            $no = 0;
-                            while ($presensi = mysqli_fetch_array($query)) {
-                                $no++;
-                            ?>
+                <div class="card-body">
+                    <div class="table-responsive">
+                        <table class="table table-striped table-sm" id="table-2" style="font-size: 12px">
+                            <thead>
                                 <tr>
-                                    <td><?= $no; ?></td>
-                                    <td><?= $presensi['nama'] ?></td>
-                                    <td><?= $presensi['nama_kelas'] ?></td>
-                                    <!-- <td><?= $presensi['tgl'] ?></td> -->
-                                    <td><?= $presensi['jam_msk'] ?></td>
-                                    <td><?= $presensi['jam_plg'] ?></td>
-                                    <td><?= $presensi['ket'] ?></td>
-                                    <!-- <td>
+                                    <th class="text-center">
+                                        #
+                                    </th>
+                                    <th>Nama Siswa</th>
+                                    <th>Kelas</th>
+                                    <!-- <th>Tanggal</th> -->
+                                    <th>Jam Masuk</th>
+                                    <th>Jam Pulang</th>
+                                    <th>Ket</th>
+                                    <!-- <th>Action</th> -->
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <?php
+                                $sekarang = date('Ymd');
+                                $query = mysqli_query($koneksi, "select * from presensi a join kelas b ON a.id_kelas=b.id_kelas where a.tgl='$sekarang'");
+                                $no = 0;
+                                while ($presensi = mysqli_fetch_array($query)) {
+                                    $no++;
+                                ?>
+                                    <tr>
+                                        <td><?= $no; ?></td>
+                                        <td><?= $presensi['nama'] ?></td>
+                                        <td><?= $presensi['nama_kelas'] ?></td>
+                                        <!-- <td><?= $presensi['tgl'] ?></td> -->
+                                        <td><?= $presensi['jam_msk'] ?></td>
+                                        <td><?= $presensi['jam_plg'] ?></td>
+                                        <td><?= $presensi['ket'] ?></td>
+                                        <!-- <td>
                                         <button data-id="<?= $presensi['id_presensi'] ?>" class="edit btn btn-danger btn-sm"><i class="fas fa-trash    "></i></button>
                                     </td> -->
-                                </tr>
-                            <?php }
-                            ?>
-                        </tbody>
-                    </table>
+                                    </tr>
+                                <?php }
+                                ?>
+                            </tbody>
+                        </table>
+                    </div>
                 </div>
             </div>
         </div>
     </div>
-</div>
+<?php } else { ?>
+    <?php
+    $tgl = dekripsi($_GET['tgl']);
+    $kelas = dekripsi($_GET['kelas']); { ?>
+        <div class="row">
+            <div class="col-12 col-sm-12 col-lg-12">
+                <div class="card shadow mb-4">
+                    <div class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
+                        <h5 class='m-0 font-weight-bold text-primary'>Presensi Siswa Hari <?= hari_ini() ?></h5>
+                        <div class="card-header-action">
+                        </div>
+                    </div>
+                    <div class="card-body">
+                        <div class="table-responsive">
+                            <table class="table table-striped table-sm" id="table-2" style="font-size: 12px">
+                                <thead>
+                                    <tr>
+                                        <th class="text-center">
+                                            #
+                                        </th>
+                                        <th>Nama Siswa</th>
+                                        <th>Kelas</th>
+                                        <th>Tanggal</th>
+                                        <th>Jam Masuk</th>
+                                        <th>Jam Pulang</th>
+                                        <th>Ket</th>
+                                        <!-- <th>Action</th> -->
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <?php
+                                    $sekarang = date('Ymd');
+                                    $query = mysqli_query($koneksi, "select * from presensi a join kelas b ON a.id_kelas=b.id_kelas where a.tgl='$tgl' AND a.id_kelas='$kelas'");
+                                    $no = 0;
+                                    while ($presensi = mysqli_fetch_array($query)) {
+                                        $no++;
+                                    ?>
+                                        <tr>
+                                            <td><?= $no; ?></td>
+                                            <td><?= $presensi['nama'] ?></td>
+                                            <td><?= $presensi['nama_kelas'] ?></td>
+                                            <td><?= $presensi['tgl'] ?></td>
+                                            <td><?= $presensi['jam_msk'] ?></td>
+                                            <td><?= $presensi['jam_plg'] ?></td>
+                                            <td><?= $presensi['ket'] ?></td>
+                                            <!-- <td>
+                                        <button data-id="<?= $presensi['id_presensi'] ?>" class="edit btn btn-danger btn-sm"><i class="fas fa-trash    "></i></button>
+                                    </td> -->
+                                        </tr>
+                                    <?php }
+                                    ?>
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    <?php } ?>
+<?php } ?>
