badeffbf9b61b73f5ceef1ee986dbf665709d043
ianaf1 <ianfatah1@gmail.com>
2021-09-23 11:10:30 +0700

    Update presensi_kelas.php

--- admin/mod_presensi/presensi_kelas.php
+++ admin/mod_presensi/presensi_kelas.php
@@ -84,13 +84,13 @@
     </div>
 <?php } else { ?>
     <?php
-    $tgl = $_GET['tgl'];
-    $kelas = $_GET['kelas']; { ?>
+    $tgl = fetch($koneksi, 'presensi', ['tgl' => $_GET['tgl']]);
+    $tgl = fetch($koneksi, 'presensi', ['tgl' => $_GET['kelas']]); { ?>
         <div class="row">
             <div class="col-12 col-sm-12 col-lg-12">
                 <div class="card shadow mb-4">
                     <div class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
-                        <h5 class='m-0 font-weight-bold text-primary'>Presensi Siswa Hari <?= hari_ini() ?></h5>
+                        <h5 class='m-0 font-weight-bold text-primary'>Presensi Kelas <?= $kelas['nama_kelas'] ?> Hari <?= $tgl['hari'] ?></h5>
                         <div class="card-header-action">
                         </div>
                     </div>
@@ -114,7 +114,7 @@
                                 <tbody>
                                     <?php
                                     $sekarang = date('Ymd');
-                                    $query = mysqli_query($koneksi, "select * from presensi a join kelas b ON a.id_kelas=b.id_kelas where a.tgl='$tgl' AND a.id_kelas='$kelas'");
+                                    $query = mysqli_query($koneksi, "select * from presensi a join kelas b ON a.id_kelas=b.id_kelas where a.tgl='$tgl[tgl]' AND a.id_kelas='$kelas[id_kelas]'");
                                     $no = 0;
                                     while ($presensi = mysqli_fetch_array($query)) {
                                         $no++;
