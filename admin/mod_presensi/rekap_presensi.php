7cda085a051e8f965990dc455b9ff05488348f87
ianaf1 <ianfatah1@gmail.com>
2021-10-13 23:37:09 +0700

    Update rekap_presensi.php

--- admin/mod_presensi/rekap_presensi.php
+++ admin/mod_presensi/rekap_presensi.php
@@ -9,7 +9,7 @@
                     </div>
                     <div class="card-body">
                         <div class="table-responsive">
-                            <table class="table table-striped table-sm" id="table-1" style="font-size: 12px">
+                            <table class="table table-striped table-sm" id="dataTable" style="font-size: 12px">
                                 <thead>
                                     <tr>
                                         <th class="text-center">
@@ -91,7 +91,7 @@
                 </div>
                 <div class="card-body">
                     <div class="table-responsive">
-                        <table class="table table-striped table-sm" id="table-2" style="font-size: 12px">
+                        <table class="table table-striped table-sm" id="dataTable" style="font-size: 12px">
                             <thead>
                                 <tr>
                                     <th class="text-center">
