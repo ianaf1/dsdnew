91fc331ade357287dbd6f0fc6898c3fa8a3e8dc4
ianaf1 <ianfatah1@gmail.com>
2021-10-25 00:34:21 +0700

    Update crud_mesin.php

--- admin/mod_rfid/crud_mesin.php
+++ admin/mod_rfid/crud_mesin.php
@@ -3,10 +3,7 @@ require("../../config/database.php");
 require("../../config/function.php");
 require("../../config/functions.crud.php");
 require("../../config/tahun.ajaran.php");
-session_start();
-if (!isset($_SESSION['id_user'])) {
-    die('Anda tidak diijinkan mengakses langsung');
-}
+
 if ($pg == 'ubah') {
     $data = [
         'kode'         => $_POST['kode'],
@@ -18,9 +15,9 @@ if ($pg == 'ubah') {
 }
 if ($pg == 'tambah') {
     $data = [
-        'nama_hari' => $_POST['nama_hari'],
-        'jam_msk'       => $_POST['jam_msk'],
-        'jam_keluar'       => $_POST['jam_keluar']
+        'kode'         => $_POST['kode'],
+        'nama'         => $_POST['nama'],
+        'mode'       => $_POST['mode']
     ];
     insert($koneksi, 'hari', $data);
     echo $exec;
