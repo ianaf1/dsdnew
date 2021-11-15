5f805a79764dd02cbe309bbe08ef7049679ae129
ianaf1 <79218845+ianaf1@users.noreply.github.com>
2021-11-15 23:40:25 +0700

    Update rfid.php

--- admin/mod_rfid/rfid.php
+++ admin/mod_rfid/rfid.php
@@ -61,7 +61,7 @@
                                                                     <select class="form-control select2" style="width: 100%" name="nis" required>
                                                                         <option value="">Pilih Siswa</option>
                                                                         <?php
-                                                                        $daftarsiswa = mysqli_query($koneksi, "select * from daftar a join kelas b on a.id_daftar=b.id_daftar");
+                                                                        $daftarsiswa = mysqli_query($koneksi, "select * from daftar a join kelas b on a.id_kelas=b.id_kelas");
                                                                         while ($siswa = mysqli_fetch_array($daftarsiswa)) {
                                                                         ?>
                                                                             <option value="<?= $siswa['nis'] ?>"><?= $siswa['nama'] ?> | <?= $siswa['nama_kelas'] ?></option>
