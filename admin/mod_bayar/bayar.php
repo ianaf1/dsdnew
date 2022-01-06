5effbc4ae043cff207ff22ed8cb1660bea9da1c3
ianaf1 <ianfatah1@gmail.com>
2022-01-06 08:38:39 +0700

    Update bayar.php

--- admin/mod_bayar/bayar.php
+++ admin/mod_bayar/bayar.php
@@ -108,13 +108,13 @@
                                     $query = mysqli_query($koneksi, "select id_daftar, nama, kelas from daftar where status='1'");
 
                                     while ($daftar = mysqli_fetch_array($query)) {
-                                        //$q2 = mysqli_query($koneksi, "select sum(jumlah) as total from biaya where id_kelas='$daftar[kelas]' AND id_semester != '$semester_aktif[id_semester]' OR thn_ajaran!='$tahun_ajaran_aktif[nama_thn_ajaran]'");
+                                        $q2 = mysqli_query($koneksi, "select sum(jumlah) as total from biaya where id_kelas='$daftar[kelas]' AND id_semester != '$semester_aktif[id_semester]' OR thn_ajaran!='$tahun_ajaran_aktif[nama_thn_ajaran]'");
                                         $q3 = mysqli_query($koneksi, "select sum(jumlah) as total from siswa_tunggakan where id_daftar='$daftar[id_daftar]'");
                                         $q4 = mysqli_query($koneksi, "select sum(jumlah) as total from bayar where id_daftar='$daftar[id_daftar]' AND id_biaya='L'");
                                         $tunggakansiswa = mysqli_fetch_array($q3);
-                                        //$biayasiswa = mysqli_fetch_array($q2);
+                                        $biayasiswa = mysqli_fetch_array($q2);
                                         $siswabayar = mysqli_fetch_array($q4);
-                                        $totaltunggakan = $tunggakansiswa['total'] - $siswabayar['total'];
+                                        $totaltunggakan = $biayasiswa['total'] + $tunggakansiswa['total'] - $siswabayar['total'];
                                         $no++;
                                     ?>
                                         <tr>
