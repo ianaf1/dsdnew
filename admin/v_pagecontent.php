be13b38810cc1732aac913eb891d631b23c5ae32
ianaf1 <ianfatah1@gmail.com>
2022-07-18 16:35:35 +0700

    Update v_pagecontent.php

--- admin/v_pagecontent.php
+++ admin/v_pagecontent.php
@@ -29,7 +29,7 @@
                 <div class="row no-gutters align-items-center">
                     <div class="col mr-2">
                         <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">Jumlah Siswa</div>
-                        <div class="h5 mb-0 font-weight-bold text-gray-800"><?= rowcount($koneksi, 'daftar', ['status' => 1 && 'kelas' == 10]) ?></div>
+                        <div class="h5 mb-0 font-weight-bold text-gray-800"><?= rowcount($koneksi, 'daftar', ['status' => 1 && 'kelas' != 13]) ?></div>
                     </div>
                     <div class="col-auto">
                         <i class="fas fa-user fa-2x text-gray-300"></i>
@@ -46,7 +46,7 @@
                 <div class="row no-gutters align-items-center">
                     <div class="col mr-2">
                         <div class="text-xs font-weight-bold text-success text-uppercase mb-1">Laki Laki</div>
-                        <div class="h5 mb-0 font-weight-bold text-gray-800"><?= mysqli_num_rows(mysqli_query($koneksi, "select * from daftar where jenkel = 'L' && status = '1' && kelas = '10'")) ?></div>
+                        <div class="h5 mb-0 font-weight-bold text-gray-800"><?= mysqli_num_rows(mysqli_query($koneksi, "select * from daftar where jenkel = 'L' && status = '1' && kelas != '13'")) ?></div>
                     </div>
                     <div class="col-auto">
                         <i class="fas fas fa-user fa-2x text-gray-300"></i>
@@ -63,7 +63,7 @@
                 <div class="row no-gutters align-items-center">
                     <div class="col mr-2">
                         <div class="text-xs font-weight-bold text-success text-uppercase mb-1">Perempuan</div>
-                        <div class="h5 mb-0 font-weight-bold text-gray-800"><?= mysqli_num_rows(mysqli_query($koneksi, "select * from daftar where jenkel = 'P' && status = '1' && kelas = '10'")) ?></div>
+                        <div class="h5 mb-0 font-weight-bold text-gray-800"><?= mysqli_num_rows(mysqli_query($koneksi, "select * from daftar where jenkel = 'P' && status = '1' && kelas != '13'")) ?></div>
                     </div>
                     <div class="col-auto">
                         <i class="fas fas fa-user fa-2x text-gray-300"></i>
